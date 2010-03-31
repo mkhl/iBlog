bfecee4f0102fdc699c82b2617ae60c5d09f2b51
Phillip Ghadir <phillip@Phillip-Ghadirs-iMac.local>
2010-04-01 00:35:21 +0200

    Atom-Feed für Blogs hinzugefügt...

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,6 +9,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
+      format.atom
       format.xml  { render :xml => @blogs }
     end
   end
