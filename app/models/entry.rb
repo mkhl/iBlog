5d3943e6db7976271dfac383245a0cebeb2e1d71
Andreas Krüger <Andreas.Krueger@innoQ.com>
2012-01-27 21:59:40 +0100

    This fixes 3021.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,8 +2,7 @@ class Entry < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 25
   
-  validates_presence_of :plans
-  validates_length_of :plans, :within => 3..1000
+  validates_length_of :plans, :maximum => 1000
   
   validates_presence_of :progress
   validates_length_of :progress, :within => 3..2000
