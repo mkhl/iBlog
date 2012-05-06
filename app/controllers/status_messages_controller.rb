34f2a4c636c484bf44718171a98cded3b1f9eff9
FND <fnd@fnd-oda.localdomain>
2012-05-06 14:36:51 +0200

    removed unused response formats for status messages

--- app/controllers/status_messages_controller.rb
+++ app/controllers/status_messages_controller.rb
@@ -3,23 +3,20 @@
 class StatusMessagesController < ApplicationController
 
   def index
-    @messages = StatusMessage.order('updated_at DESC').page(params[:page])
+    @messages = StatusMessage.order("updated_at DESC").page(params[:page])
     render :text => @messages.map { |msg| "[#{msg.updated_at}] #{msg.author}: #{msg.body}" }.join("\n<hr>\n") # XXX: DEBUG
   end
 
   def create
     @msg = StatusMessage.new(params[:status_message])
     @msg.author = @user
-    respond_to do |format|
-      if @msg.save
-        flash[:success] = 'Die Nachricht wurde gespeichert.'
-        format.html { redirect_to status_messages_path }
-        format.xml  { render :xml => @msg, :status => :created, :location => @msg }
-      else
-        flash[:error] = 'Die Nachricht konnte nicht gespeichert werden.'
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @msg.errors, :status => :unprocessable_entity }
-      end
+
+    if @msg.save
+      flash[:success] = "Die Nachricht wurde gespeichert."
+       redirect_to status_messages_path
+    else
+      flash[:error] = "Die Nachricht konnte nicht gespeichert werden."
+      render :action => "edit"
     end
   end
 
