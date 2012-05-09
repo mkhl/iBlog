87a5156aa785f4ca44d11d80ad52abb4ecb6058d
Robert Glaser <mail@robert-glaser.de>
2012-05-09 15:32:23 +0200

    fucking ruby 1.8. can we please have 1.9.3 on every production system?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,58 +6,59 @@ class ApplicationController < ActionController::Base
 
   protected
     def set_nav_items
-      @nav_side_items ||= {
-        "Blogs" => [
-          {
-            :path    => Proc.new { blogs_by_owner_path(@user) },
-            :icon    => "user",
-            :title   => "Meine",
-            :active? => params[:controller] == "blogs" && params[:action] == "index" && params[:owner]
-          },
-          {
-            :path    => blogs_path,
-            :icon    => "th-list",
-            :title   => "Alle",
-            :active? => params[:controller] == "blogs" && params[:action] == "index" && !params[:owner]
-          }
-        ],
-        "Posts" => [
-          {
-            :path    => Proc.new { blog_entries_by_author_path(@user) },
-            :icon    => "user",
-            :title   => "Meine",
-            :active? => params[:controller] == "entries" && params[:action] == "user_home" && params[:author]
-          },
-          {
-            :path    => entries_path,
-            :icon    => "fire",
-            :title   => "Letzte",
-            :active? => params[:controller] == "entries" && params[:action] == "full"
-          }
-        ],
-        "Feeds" => [
-          {
-            :path    => blogs_path(:format => :atom),
-            :icon    => "fire",
-            :title   => "Blogs",
-            :active? => false
-          },
-          {
-            :path    => entries_path(:format => :atom),
-            :icon    => "fire",
-            :title   => "Einträge",
-            :active? => false
-          }
-        ],
-        "Status-Nachrichten" => [
-          {
-            :path    => status_messages_path,
-            :icon    => "headphones",
-            :title   => "Letzte",
-            :active? => params[:controller] == "status_messages" && params[:action] == "index"
-          }
-        ]
-      }
+      return @nav_items if @nav_items
+      @nav_side_items = ActiveSupport::OrderedHash.new
+      @nav_side_items["Blogs"] = [
+        {
+          :path    => Proc.new { blogs_by_owner_path(@user) },
+          :icon    => "user",
+          :title   => "Meine",
+          :active? => params[:controller] == "blogs" && params[:action] == "index" && params[:owner]
+        },
+        {
+          :path    => blogs_path,
+          :icon    => "th-list",
+          :title   => "Alle",
+          :active? => params[:controller] == "blogs" && params[:action] == "index" && !params[:owner]
+        }
+      ]
+      @nav_side_items["Posts"] = [
+        {
+          :path    => Proc.new { blog_entries_by_author_path(@user) },
+          :icon    => "user",
+          :title   => "Meine",
+          :active? => params[:controller] == "entries" && params[:action] == "user_home" && params[:author]
+        },
+        {
+          :path    => entries_path,
+          :icon    => "fire",
+          :title   => "Letzte",
+          :active? => params[:controller] == "entries" && params[:action] == "full"
+        }
+      ]
+      @nav_side_items["Feeds"] = [
+        {
+          :path    => blogs_path(:format => :atom),
+          :icon    => "fire",
+          :title   => "Blogs",
+          :active? => false
+        },
+        {
+          :path    => entries_path(:format => :atom),
+          :icon    => "fire",
+          :title   => "Einträge",
+          :active? => false
+        }
+      ]
+      @nav_side_items["Status-Nachrichten"] = [
+        {
+          :path    => status_messages_path,
+          :icon    => "headphones",
+          :title   => "Letzte",
+          :active? => params[:controller] == "status_messages" && params[:action] == "index"
+        }
+      ]
+      @nav_side_items
     end
 
     def set_user
