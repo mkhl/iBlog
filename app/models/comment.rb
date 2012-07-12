4f111e618eced6578c991201f66e2a6f02fc5945
Robert Glaser <mail@robert-glaser.de>
2012-07-12 13:42:59 +0200

    added markdown support for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :entry
+
+  before_save :regenerate_html
+
+  def regenerate_html
+    options = Rails.application.config.redcarpet_options
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:hard_wrap => true), options)
+
+    self.content = markdown.render(content)
+  end
 end
