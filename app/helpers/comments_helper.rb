9dffec6351de833a06f6de698598308170536170
Andreas Krüger <Andreas.Krueger@innoQ.com>
2012-07-27 12:27:24 +0200

    This fixes #41 - do the simplest thing that could possibly work.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
   def avatar(user)
-    image_tag("https://internal.innoq.com/iqumf/images/users/thm-#{user}.jpg", :class => "avatar")
+    image_tag("https://internal.innoq.com/iqumf/images/users/thm-#{user}.jpg", :class => "avatar", :alt => "?")
   end
 end
