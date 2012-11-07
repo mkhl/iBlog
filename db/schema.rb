8e8edaea812eb4a3df45002b2aa80db67c71e2e7
iblog innoQ deployuser <iblog@internal.innoq.com>
2012-11-07 16:57:21 +0100

    RAILS_ENV=production bundle exec rake db:schema:dump # run by Andreas Krüger

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -35,7 +34,7 @@ ActiveRecord::Schema.define(:version => 20121020011733) do
 
   create_table "entries", :force => true do |t|
     t.string   "title"
-    t.text     "body",          :limit => 16777215
+    t.string   "body",          :limit => 24000
     t.string   "excerpt",       :limit => 1000
     t.string   "author"
     t.datetime "created_at"
