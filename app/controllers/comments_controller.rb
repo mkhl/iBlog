53cdee3ad57fec09b838b962dab79fb49a80ac42
FND <fnd@localhost.localdomain>
2013-05-17 11:16:43 +0200

    switched to dates instead of timestamps
    as suggested by @mrreynolds

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
     ## TODO
     # Feeds for: blog comments, entry comments
     @comments = Comment.includes(:entry => :blog).
-        where(:updated_at => 40.days.ago..Time.now)
+        where(:updated_at => 40.days.ago.to_date..Date.today)
 
     respond_to do |format|
       format.html do
