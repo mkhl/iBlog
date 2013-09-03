8e4c82c19d0ef5ce97d021d2b2e589f6b9095b4c
Stefan Tilkov <stefan.tilkov@innoq.com>
2013-09-03 23:09:00 +0200

    Very basic support for sending out weekly emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,8 @@ Iblog::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -44,4 +44,7 @@ Iblog::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :sendmail
+  
 end
