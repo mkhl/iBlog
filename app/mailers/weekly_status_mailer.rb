abdf4d76b25fd8b8e6aff94c43b48c2d33be4d0e
Stefan Tilkov <stefan.tilkov@innoq.com>
2013-09-04 16:05:42 +0200

    fuck Ruby 1.8

--- app/mailers/weekly_status_mailer.rb
+++ app/mailers/weekly_status_mailer.rb
@@ -1,7 +1,7 @@
 class WeeklyStatusMailer < ActionMailer::Base
   helper :comments, :entries
-  default from: 'notifications@innoq.com'
- 
+  default {:from => 'notifications@innoq.com'}
+
   def weekly_status_email(recipient)
   	@week = Time.now.strftime('%W')
     @statuses = WeeklyStatus.by_week(@week).recent
