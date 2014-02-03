17f8dd2b39fe47edfa95f2f68ab57ac8a5031681
FND <fnd@localhost.localdomain>
2014-02-03 07:25:56 +0100

    added option to specify host URL for weekly status mailer
    since there's no explicit configuration to read/infer from, this appears
    to be the only way
    
    there's probably some way to modify some Rails setting to use `url_for`
    instead of manually concatenating host and path in the view, but I don't
    what that is

--- app/mailers/weekly_status_mailer.rb
+++ app/mailers/weekly_status_mailer.rb
@@ -2,7 +2,8 @@ class WeeklyStatusMailer < ActionMailer::Base
   helper :comments, :entries
   default :from => 'notifications@innoq.com'
 
-  def weekly_status_email(recipient)
+  def weekly_status_email(recipient, host)
+    @host = host.chomp('/')
     @week = Time.now.strftime('%W')
     @statuses = WeeklyStatus.by_week(@week).recent
     mail({:to => recipient, :subject => "innoQ Wochenstatus (KW #{@week})"})
--- lib/tasks/weeklymail.rake
+++ lib/tasks/weeklymail.rake
@@ -1,6 +1,6 @@
 namespace :weeklymail do
   desc "Sends out weekly status report"
-  task :deliver, [:recipient] => [:environment] do |t, args|
-    WeeklyStatusMailer.weekly_status_email(args.recipient).deliver
+  task :deliver, [:recipient, :host] => [:environment] do |t, args|
+    WeeklyStatusMailer.weekly_status_email(args.recipient, args.host).deliver
   end
 end
