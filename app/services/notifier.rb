d488d9a0b460545ed434bd4c26c96b4e809007cb
FND <fnd@localhost.localdomain>
2014-11-10 07:47:06 +0100

    Revert "added support for basic auth"
    the Authorization header is already being used for the bearer token,
    thus Basic Auth doesn't make sense
    
    This reverts commit 49f2d069e960b1412faec6d4f5d24baa544cbe8a.

--- app/services/notifier.rb
+++ app/services/notifier.rb
@@ -4,8 +4,6 @@ require "uri"
 class Notifier
 
   def self.dispatch(recipients, subject, body)
-    username = ENV["IBLOG_NAVEED_USERNAME"]
-    password = ENV["IBLOG_NAVEED_PASSWORD"]
     naveed = ENV["IBLOG_NAVEED_URL"]
     token = ENV["IBLOG_NAVEED_TOKEN"]
     return false unless naveed && token
@@ -14,7 +12,6 @@ class Notifier
     http = Net::HTTP.new(naveed.host, naveed.port)
 
     req = Net::HTTP::Post.new(naveed.request_uri)
-    req.basic_auth(username, password) if username && password
     req.add_field("Authorization", "Bearer #{token}")
     req.set_form_data({
       "recipient" => recipients,
