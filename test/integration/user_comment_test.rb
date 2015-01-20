4df0a109a737fe244624f9d5d8c0b5308da6406a
Marc Jansing <marc.jansing@innoq.com>
2015-01-20 19:02:20 +0100

    add comment integration test

---
+++ test/integration/user_comment_test.rb
@@ -0,0 +1,45 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), '../integration_test_helper')
+
+class UserCommentTest < ActionDispatch::IntegrationTest
+  test 'weekly status comment creation' do
+    status = WeeklyStatus.new(status: 'pgs current status').tap do |w|
+      w.author = 'pg'
+      w.save
+    end
+
+    visit weekly_status_path(status)
+    assert_content 'pgs current status'
+    assert_content 'Kommentare: 0'
+
+    fill_in 'comment_content', with: 'comment on pgs current status'
+    click_button 'Kommentar abgeben'
+
+    assert_content 'Der Kommentar wurde gespeichert.'
+    assert_content 'comment on pgs current status'
+
+    visit weekly_statuses_path
+    assert_content 'pgs current status'
+    assert_content 'Kommentare: 1'
+  end
+
+  test 'entry comment creation' do
+    blog = Blog.create(name: 'pgs blog')
+    entry = Entry.new(title: 'pgs sample ppp', progress: 'pgs sample progress').tap do |e|
+      e.author = 'pg'
+      e.blog = blog
+      e.save
+    end
+
+    visit blog_entry_path(blog, entry)
+    assert_content 'pgs sample ppp'
+    assert_content 'pgs sample progress'
+    assert_content 'Kommentare: 0'
+    fill_in 'comment_content', with: 'comment on pgs sample progress'
+    click_button 'Kommentar abgeben'
+
+    assert_content 'Der Kommentar wurde gespeichert.'
+    assert_content 'pgs sample ppp'
+    assert_content 'Kommentare: 1'
+    assert_content 'comment on pgs sample progress'
+  end
+end
