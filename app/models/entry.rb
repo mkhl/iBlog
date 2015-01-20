9921a077aa95fa3aadecac0593224848f17eecc7
Marc Jansing <marc.jansing@innoq.com>
2015-01-20 10:19:06 +0100

    move modules into concerns dir

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-require "modules/markdown"
-require "modules/authored"
+require 'concerns/markdown'
+require 'concerns/authored'
 class Entry < ActiveRecord::Base
   include Markdown
   include Authored
