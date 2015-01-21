b5bce6666c0e84badd5101a6ff19e80b56133ef2
Marc Jansing <marc.jansing@innoq.com>
2015-01-21 14:58:26 +0100

    add foreign key constraints

--- db/schema.rb
+++ db/schema.rb
@@ -9,64 +9,70 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130805095507) do
+ActiveRecord::Schema.define(version: 20150121132524) do
 
-  create_table "blogs", :force => true do |t|
-    t.string   "name"
-    t.string   "owner"
-    t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "title"
+  create_table "blogs", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "owner",       limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "title",       limit: 255
   end
 
-  create_table "comments", :force => true do |t|
-    t.integer  "owner_id"
-    t.string   "author"
-    t.text     "content"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.text     "content_html"
-    t.string   "owner_type"
+  create_table "comments", force: :cascade do |t|
+    t.integer  "owner_id",     limit: 4
+    t.string   "author",       limit: 255
+    t.text     "content",      limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.text     "content_html", limit: 65535
+    t.string   "owner_type",   limit: 255
   end
 
-  add_index "comments", ["owner_id", "owner_type"], :name => "index_comments_on_owner_id_and_owner_type"
-  add_index "comments", ["owner_id"], :name => "index_comments_on_entry_id"
+  add_index "comments", ["owner_id", "owner_type"], name: "index_comments_on_owner_id_and_owner_type", using: :btree
+  add_index "comments", ["owner_id"], name: "index_comments_on_entry_id", using: :btree
 
-  create_table "entries", :force => true do |t|
-    t.string   "title"
-    t.string   "author"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.text     "progress"
-    t.text     "plans"
-    t.text     "problems"
-    t.integer  "blog_id"
-    t.text     "progress_html"
-    t.text     "plans_html"
-    t.text     "problems_html"
+  create_table "entries", force: :cascade do |t|
+    t.string   "title",         limit: 255
+    t.string   "author",        limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.text     "progress",      limit: 65535
+    t.text     "plans",         limit: 65535
+    t.text     "problems",      limit: 65535
+    t.integer  "blog_id",       limit: 4
+    t.text     "progress_html", limit: 65535
+    t.text     "plans_html",    limit: 65535
+    t.text     "problems_html", limit: 65535
   end
 
-  create_table "status_messages", :force => true do |t|
-    t.string   "author"
-    t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  add_index "entries", ["blog_id"], name: "fk_rails_60c6189354", using: :btree
+
+  create_table "status_messages", force: :cascade do |t|
+    t.string   "author",     limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "tags", :force => true do |t|
-    t.string  "name"
-    t.integer "entry_id"
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",     limit: 255
+    t.integer "entry_id", limit: 4
   end
 
-  create_table "weekly_statuses", :force => true do |t|
-    t.string   "author"
-    t.text     "status"
-    t.text     "status_html"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+  add_index "tags", ["entry_id"], name: "fk_rails_afdbd3665f", using: :btree
+
+  create_table "weekly_statuses", force: :cascade do |t|
+    t.string   "author",      limit: 255
+    t.text     "status",      limit: 65535
+    t.text     "status_html", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
+  add_foreign_key "entries", "blogs", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "tags", "entries", on_update: :cascade, on_delete: :cascade
 end
