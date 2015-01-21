a92d9e6875179a2fdd093f15a48c34a1ec319c10
Marc Jansing <marc.jansing@innoq.com>
2015-01-21 14:14:49 +0100

    rails v4.2 config

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,9 @@ module Iblog
     config.i18n.available_locales = :de
     config.i18n.default_locale = :de
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.redcarpet_options = { :autolink => true, :fenced_code => true, :gh_blockcode => true }
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
