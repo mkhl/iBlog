10a405f12130dbfb4b5a3cf77089bbfa42aa150b
Marc Jansing <marc.jansing@innoq.com>
2015-04-24 16:57:56 +0200

    show absolute comments creation time as a tooltip

--- app/assets/javascripts/iblog.js
+++ app/assets/javascripts/iblog.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
   $(".comment-timestamp time[datetime]").each(function(idx, item) {
       var time = moment($(item).attr("datetime")).local();
       $(item).attr("title", time.format("dddd, Do MMMM YYYY [um] HH:mm:ss")).text(time.fromNow());
+
+      // show title (absolute time) as a bootstrap tooltip
+      $(item).data({
+        "toggle": "tooltip",
+        "placement": "top"
+      });
+      $(item).tooltip();
   });
 });
